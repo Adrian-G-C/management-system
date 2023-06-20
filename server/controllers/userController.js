dc84f7ea9853a6f450be283ee57ff10d2c0b3a15
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-20 17:27:46 -0500

    updated controller to remove banned users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,14 +2,11 @@ const mysql = require('mysql');
 
 // View Users
 exports.view = (req, res) => {
-    
-
     pool.getConnection((err, connection) => {
     if(err) throw err; //not connected
     console.log('Connected as ID ' + connection.threadID);
-
     //User the connection
-    connection.query('SELECT * FROM user', (err, rows) => {
+    connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
         // when done with the connection, releast it
         connection.release();
         if(!err) {
@@ -17,9 +14,7 @@ exports.view = (req, res) => {
         } else {
           console.log(err)  
         }
-
         console.log('The data from user table \n', rows);
     }); 
 });
-
 }
