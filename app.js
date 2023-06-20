0081ce4456ee3cf313036b148f243f2d1f3e6061
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-20 16:50:32 -0500

    added routes and requirements to user.js

--- app.js
+++ app.js
@@ -24,9 +24,22 @@ const handlebars = exphbs.create({ extname: '.hbs',});
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 
-//Router
-app.get('', (req, res) => {
-    res.render('home');
+// Connection Pool
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASS,
+    database        : process.env.DB_NAME
 });
+
+// Connect to DB
+pool.getConnection((err, connection) => {
+    if(err) throw err; //not connected
+    console.log('Connected as ID ' + connection.threadID);
+});
+
+const routes = require('./server/routes/user');
+app.use('/', routes);
 
 app.listen(port, () => console.log(`Listening on port ${3003}`));
