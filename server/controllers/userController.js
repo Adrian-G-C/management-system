81c7ddecbaac3ccb1d5bded08d55465e3e488fd3
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-20 18:01:20 -0500

    ability to add new users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,9 +24,32 @@ exports.find = (req, res) => {
     let searchTerm = req.body.search;
     // User the connection
     connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
-      connection.release();
+      // When done with connection release it.
+        connection.release();
         if (!err) {
         res.render('home', { rows });
+      } else {
+        console.log(err);
+      }
+      console.log('The data from user table: \n', rows);
+    });
+  }
+
+  exports.form = (req, res) => {
+    res.render('add-user');
+  }
+
+  // Add new user
+  exports.create = (req, res) => {
+    const { first_name, last_name, email, phone, comments } = req.body;
+    let searchTerm = req.body.search;
+  
+    // User the connection
+    connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
+      // When done with connection release it.
+      connection.release();
+        if (!err) {
+        res.render('add-user', { alert: 'User added successfully.' });
       } else {
         console.log(err);
       }
