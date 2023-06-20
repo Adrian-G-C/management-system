cf0e54a23e37fcb382787c10046c8dca854cf4af
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-20 17:05:26 -0500

    connected user controllers file

---
+++ server/controllers/userController.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+// View Users
+exports.view = (req, res) => {
+    
+
+    pool.getConnection((err, connection) => {
+    if(err) throw err; //not connected
+    console.log('Connected as ID ' + connection.threadID);
+
+    //User the connection
+    connection.query('SELECT * FROM user', (err, rows) => {
+        // when done with the connection, releast it
+        connection.release();
+        if(!err) {
+            res.render('home', { rows });
+        } else {
+          console.log(err)  
+        }
+
+        console.log('The data from user table \n', rows);
+    }); 
+});
+
+}
