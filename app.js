e19e3ee3f02a5ad02b302d413def445de36dd6e8
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-20 14:34:43 -0500

    templating engine setup

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false}));
 // Parse application/json
 app.use(bodyParser.json());
 
+// Static Files
 app.use(express.static('public'));
+
+// Template Engine
+app.engine('hbs', exphbs( {extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 app.listen(port, () => console.log(`Listening on port ${3003}`));
