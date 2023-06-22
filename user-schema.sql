ee9a1c231da1bb4208a24716b8e6a85cab18e9e2
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-22 17:25:41 -0500

    original schema

--- user-schema.sql
+++ user-schema.sql
@@ -1,16 +1,1 @@
-DROP DATABASE IF EXISTS usermanagement_db;
-CREATE DATABASE usermanagement_db;
-
-USE usermanagement_db;
-
-CREATE TABLE user ( 
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(45) NOT NULL,
-    last_name VARCHAR(45) NOT NULL,
-    email VARCHAR(45) NOT NULL,
-    phone VARCHAR(45) NOT NULL,
-    comments TEXT NOT NULL,
-    status VARCHAR(10) NOT NULL DEFAULT 'active'
-    );
-
-    ENGINE = InnoDB ;
+CREATE TABLE `usermanagement_tut`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'active' , PRIMARY KEY (`id`)) ENGINE = InnoDB;
