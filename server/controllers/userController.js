fccbf50ec95dd7ddf42e24454025f340e0bfd03a
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-22 16:55:27 -0500

    delete user finished

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -69,3 +69,42 @@ exports.edit = (req, res) => {
       console.log('The data from user table: \n', rows);
     });
   }
+
+// Update User
+exports.update = (req, res) => {
+    const { first_name, last_name, email, phone, comments } = req.body;
+    // User the connection
+    connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
+  
+      if (!err) {
+        // User the connection
+        connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+          // When done with the connection, release it
+          
+          if (!err) {
+            res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
+          } else {
+            console.log(err);
+          }
+          console.log('The data from user table: \n', rows);
+        });
+      } else {
+        console.log(err);
+      }
+      console.log('The data from user table: \n', rows);
+    });
+  }
+
+  // Delete User
+exports.delete = (req, res) => {
+  connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
+    if (!err) {
+      let removedUser = encodeURIComponent('User successeflly removed.');
+      res.redirect('/?removed=' + removedUser);
+    } else {
+      console.log(err);
+    }
+    console.log('The data from beer table are: \n', rows);
+  });
+
+}
