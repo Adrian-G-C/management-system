5f891164415c39814efe45b638f38c74f206950e
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-22 20:03:45 -0500

    latest

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 3306;
+const port = process.env.PORT || 3002;
 
 // Parsing middleware
 // Pars application/x-wwww-form-urlencoded
@@ -25,19 +25,19 @@ app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 
 // Connection Pool
-const pool = mysql.createPool({
-    connectionLimit : 100,
-    host            : process.env.DB_HOST,
-    user            : process.env.DB_USER,
-    password        : process.env.DB_PASS,
-    database        : process.env.DB_NAME
-});
+// const pool = mysql.createPool({
+//     connectionLimit : 100,
+//     host            : process.env.DB_HOST,
+//     user            : process.env.DB_USER,
+//     password        : process.env.DB_PASS,
+//     database        : process.env.DB_NAME
+// });
 
 // Connect to DB
-pool.getConnection((err, connection) => {
-    if(err) throw err; //not connected
-    console.log('Connected as ID ' + connection.threadID);
-});
+// pool.getConnection((err, connection) => {
+//     if(err) throw err; //not connected
+//     console.log('Connected as ID ' + connection.threadID);
+// });
 
 const routes = require('./server/routes/user');
 app.use('/', routes);
